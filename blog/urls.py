aae5264cf63a2fd2ecd52db9c59112c4032db596
Mzzj114 <1174931367@qq.com>
2024-02-13 10:53:15 +0800

    update: ui refine add searchbar on front-end

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import path, reverse
 from . import views
 
 app_name = "blog"
 urlpatterns = [
     path("index", views.index, name="index"),
     path("posts/<str:posttitle>", views.posts, name="posts"),
-    path("", views.index)
+    path("", lambda request: redirect(reverse('index'))),
 ]
